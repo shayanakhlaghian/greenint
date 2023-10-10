aaf6ca7b7838ee5c001efe65361ea9c10578ce83
Shayan <shayanakhlaghian@gmail.com>
2023-10-10 10:20:18 +0330

    do minor styling fixes

--- app/get/components/Nav.tsx
+++ app/get/components/Nav.tsx
@@ -2,7 +2,7 @@ import NavItem from './NavItem';
 
 const Nav = () => {
   return (
-    <nav className='w-5/6 md:w-1/3 border-2 rounded-full fixed top-5 left-1/2 translate-x-[-50%] py-1 px-2'>
+    <nav className='w-5/6 md:w-1/3 border-2 bg-primary rounded-full fixed top-5 left-1/2 translate-x-[-50%] py-1 px-2'>
       <ul className='flex justify-around items-center'>
         <NavItem href='/get/data'>Get Data</NavItem>
         <NavItem href='/get/code'>Get Code</NavItem>
